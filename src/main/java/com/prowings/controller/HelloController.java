270ce6735a5a757b32444fbdfc6ec020a5e602dd
JD <jayprowings@gmail.com>
2023-01-06 13:37:09 +0530

    initial draft

---
+++ src/main/java/com/prowings/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.prowings.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello")  
+    public String display()  
+    {  
+//		if(true)
+//			throw new NullPointerException();
+        return "hello";  
+    }     
+	
+}
